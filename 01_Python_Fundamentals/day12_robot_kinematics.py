54ea1cb5f112dac81b220092d859017cbf8af74c
Akash Parmar <akashparmar87585@gmail.com>
2025-12-17 08:55:28 +0530

    day 12 robot kinmatics

---
+++ 01_Python_Fundamentals/day12_robot_kinematics.py
@@ -0,0 +1,29 @@
+import math 
+import time
+
+# intial postion
+
+x,y=0,0
+thet_deg=0  
+theat= thet_deg* math.pi/180
+
+#robot speed
+v=0.5  #m/s linear spedd
+omega= math.pi/4  # rad/s angular speed
+dt=1  #1 sec delay
+
+
+print("🤖 Robot Kinematics Simulation Started\n")
+
+for step in range(5):
+#update position
+    x += v* math.cos(theat)*dt
+y += v* math.sin(theat)*dt
+
+
+#update orientation
+theat += omega *dt
+
+print(f"step {step+1}: x={x:2f}, y={y:2f}, theat={math.degrees(theat):2f}")
+
+time.sleep(3)
